dc085aeab2805707a3755e63302a97e6880b0463
ericaphillips <ericaphill@gmail.com>
2021-03-16 11:34:14 -0500

    finished auth without pharmacy

---
+++ src/components/auth/Register.js
@@ -0,0 +1,111 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import "./auth.css"
+
+export const Register = (props) => {
+    const firstName = React.createRef()
+    const lastName = React.createRef()
+    const email = React.createRef()
+    const phone = React.createRef()
+    const zipcode = React.createRef()
+    const password = React.createRef()
+    const verifyPassword = React.createRef()
+    const passwordDialog = React.createRef()
+
+    const handleRegister = (e) => {
+        e.preventDefault()
+
+        if (password.current.value === verifyPassword.current.value) {
+            const newUser = {
+                "username": email.current.value,
+                "first_name": firstName.current.value,
+                "last_name": lastName.current.value,
+                "phone": phone.current.value,
+                "zipcode": zipcode.current.value,
+                "email": email.current.value,
+                "password": password.current.value
+            }
+
+            return fetch("http://127.0.0.1:8000/register", {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                    "Accept": "application/json"
+                },
+                body: JSON.stringify(newUser)
+            })
+                .then(res => res.json())
+                .then(res => {
+                    if ("token" in res) {
+                        localStorage.setItem("rare_token", res.token)
+                        props.history.push("/")
+                    }
+                })
+        } else {
+            passwordDialog.current.showModal()
+        }
+    }
+
+    return (
+        <main style={{ textAlign: "center" }}>
+
+            <dialog className="dialog dialog--password" ref={passwordDialog}>
+                <div>Passwords do not match</div>
+                <button className="button--close" onClick={e => passwordDialog.current.close()}>Close</button>
+            </dialog>
+
+            <form className="form--login" onSubmit={handleRegister}>
+                <h1 className="h3 mb-3 font-weight-normal">Register an account</h1>
+                {/* <fieldset> */}
+                <div>
+                    <label htmlFor="firstName"> First Name </label>
+                    <input ref={firstName} type="text" name="firstName" className="form-control" placeholder="First name" required autoFocus />
+                </div>
+                {/* </fieldset>
+                <fieldset> */}
+                <div>
+                    <label htmlFor="lastName"> Last Name </label>
+                    <input ref={lastName} type="text" name="lastName" className="form-control" placeholder="Last name" required />
+                </div>
+                {/* </fieldset>
+                <fieldset> */}
+                <div>
+                    <label htmlFor="inputEmail"> Email address </label>
+                    <input ref={email} type="email" name="email" className="form-control" placeholder="Email address" required />
+                </div>
+                {/* </fieldset>
+                <fieldset> */}
+                <div>
+                    <label htmlFor="inputPassword"> Password </label>
+                    <input ref={password} type="password" name="password" className="form-control" placeholder="Password" required />
+                </div>
+                {/* </fieldset>
+                <fieldset> */}
+                <div>
+                    <label htmlFor="verifyPassword"> Verify Password </label>
+                    <input ref={verifyPassword} type="password" name="verifyPassword" className="form-control" placeholder="Verify password" required />
+                </div>
+                {/* </fieldset>
+                <fieldset> */}
+                <div>
+                    <label htmlFor="phone"> Phone Number </label>
+                    <textarea ref={phone} name="phone" className="form-control" placeholder="Phone number" />
+                </div>
+                {/* </fieldset> */}
+                <div>
+                <label htmlFor="zipcode"> Zipcode </label>
+                    <textarea ref={zipcode} name="zipcode" className="form-control" placeholder="Zip" />
+                </div>
+                <div style={{
+                    textAlign: "center"
+                }}>
+                    <button className="btn btn-1 btn-sep icon-send" type="submit">Register</button>
+                </div>
+            </form>
+            <section className="link--register">
+                Already registered? <Link to="/login">Login</Link>
+            </section>
+        </main>
+    )
+}
+
