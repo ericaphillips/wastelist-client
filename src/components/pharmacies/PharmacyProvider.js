3e8eacc78ca2555dff5ee56852c7992653df682a
ericaphillips <ericaphill@gmail.com>
2021-03-17 10:07:13 -0500

    pharmacy providers done

---
+++ src/components/pharmacies/PharmacyProvider.js
@@ -0,0 +1,65 @@
+import React, { useState } from "react"
+
+export const PharmacyContext = React.createContext()
+
+export const PharmacyProvider = (props) => {
+    const [pharmacies, setPharmacies] = useState([])
+
+    const getPharmacies = () => {
+        return fetch("http://localhost:8000/pharmacies", {
+            headers:{
+                "Authorization": `Token ${localStorage.getItem("waste_token")}`     
+            }       
+        })
+            .then(res => res.json())
+            .then(setPharmacies)
+    }
+
+    const addPharmacy = pharmacy => {
+        return fetch("http://localhost:8000/pharmacies", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("waste_token")}`  
+            },
+            body: JSON.stringify(pharmacy)
+        })
+            .then(getPharmacies)
+    }
+
+    const removePharmacy = pharmacyId => {
+        return fetch(`http://localhost:8000/pharmacies/${pharmacyId}`, {
+            method: "DELETE",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": `Token ${localStorage.getItem("waste_token")}`  
+            },
+        })
+            .then(getCategories)
+    }
+
+    const updatePharmacy = pharmacy => {
+        return fetch(`http://localhost:8000/pharmacies/${pharmacy.id}`, {
+            method: "PUT",
+            headers: {
+                "Authorization": `Token ${localStorage.getItem("waste_token")}`,  
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify(pharmacy)
+        })
+            .then(getPharmacies)
+    }
+
+    /*
+        You return a context provider which has the
+        `pharmacies` state and the  function as keys. This
+        allows any child elements to access them.
+    */
+    return (
+        <PharmacyContext.Provider value={{
+            pharmacies, addPharmacy, getPharmacies, removePharmacy, updatePharmacy
+        }}>
+            {props.children}
+        </PharmacyContext.Provider>
+    )
+}
