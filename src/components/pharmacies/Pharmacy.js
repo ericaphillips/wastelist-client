bdcc5b99e6c090ec1098a126044521f72c1fc23e
ericaphillips <ericaphill@gmail.com>
2021-03-18 14:32:33 -0500

    list started not working

---
+++ src/components/pharmacies/Pharmacy.js
@@ -0,0 +1,39 @@
+import React, { useContext } from "react"
+import { PharmacyContext } from "../pharmacies/PharmacyProvider"
+import { useHistory } from "react-router-dom"
+// import './Pharmacy.css'
+
+// component responsible for rendering a single pharmacy
+
+export const Pharmacy = ({ pharmacy }) => {
+    const { removePharmacy } = useContext(PharmacyContext)
+    const history = useHistory()
+
+    const confirmDelete = (id) => {
+        const d = window.confirm("Would you like to delete this?")
+        if (d === true) {
+            removePharmacy(id)
+        }
+    }
+
+    return (<section className="pharmacy">
+        <h3 className="pharmacy--name">{pharmacy.name}</h3>
+        <div className="pharmacy--address">{pharmacy.address}</div>
+        <div className="pharmacy--zipcode">{pharmacy.zipcode}</div>
+        <div className="pharmacy--appointment_hours">{pharmacy.appointmet_hours}</div>
+        <div className="buttons">
+            <button className="button--delete" onClick={
+                () => {
+                    confirmDelete(pharmacy.id)
+                }
+            }>
+                Delete Pharmacy Listing
+            </button>
+            <button className="button--edit" onClick={() => {
+                history.push(`/pharmacies/edit/${pharmacy.id}`)
+            }}>Edit Pharmacy Listing
+            </button>
+        </div>
+    </section>
+    )
+}
